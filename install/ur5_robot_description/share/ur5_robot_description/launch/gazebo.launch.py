4e20d3f4123e0dfe7070c1883312fcabc8eb8a2d
Tajish <farhantajish@gmail.com>
2025-01-21 02:12:37 +1100

    modified

---
+++ install/ur5_robot_description/share/ur5_robot_description/launch/gazebo.launch.py
@@ -0,0 +1,76 @@
+import os
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description():
+    ur5_robot_description = get_package_share_directory("ur5_robot_description")
+
+    model_arg = DeclareLaunchArgument(name="model", default_value=os.path.join(
+                                        ur5_robot_description, "urdf", "ur5_gripper_main.xacro"
+                                        ),
+                                      description="Absolute path to robot urdf file"
+    )
+
+    gazebo_resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH",
+        value=[
+            str(Path(ur5_robot_description).parent.resolve())
+            ]
+        )
+    
+    ros_distro = os.environ["ROS_DISTRO"]
+
+    physics_engine = "" if ros_distro == "iron" else "--physics-engine gz-physics-bullet-featherstone-plugin"
+
+    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]),
+                                       value_type=str)
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description,
+                     "use_sim_time": True}]
+    )
+
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
+                launch_arguments=[
+                    ("gz_args", [" -v 4 -r empty.sdf ", physics_engine]
+                    )
+                ]
+             )
+
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=["-topic", "robot_description",
+                   "-name", "ur5_robot"],
+    )
+
+    gz_ros2_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+        ]
+    )
+
+    return LaunchDescription([
+        model_arg,
+        gazebo_resource_path,
+        robot_state_publisher_node,
+        gazebo,
+        gz_spawn_entity,
+        gz_ros2_bridge
+    ])
---
+++ src/ur5_robot_description/launch/gz.launch.py
@@ -0,0 +1,80 @@
+import os
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable, IncludeLaunchDescription
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description():
+    ur5_robot_description = get_package_share_directory("ur5_robot_description")
+
+    model_arg = DeclareLaunchArgument(
+        name="model",
+        default_value=os.path.join(
+            ur5_robot_description, "urdf", "ur5_gripper_main.xacro"
+        ),
+        description="Absolute path to robot URDF file"
+    )
+
+    ros_distro = os.environ["ROS_DISTRO"]
+    is_classic = "True" if ros_distro == "iron" else False
+
+    gazebo_resource_path = SetEnvironmentVariable(
+        name="GAZEBO_MODEL_PATH",
+        value=[
+            str(Path(ur5_robot_description).parent.resolve())
+        ]
+    )
+
+    robot_description = ParameterValue(
+        Command(["xacro ", LaunchConfiguration("model"),
+        " is_classic:=",
+        is_classic
+        ]),
+        value_type=str
+    )
+
+    # Robot State Publisher
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{
+            "robot_description": robot_description,
+            "use_sim_time": True
+        }]
+    )
+
+    # Launch Gazebo Classic
+    gazebo_classic = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory("gazebo_ros"), "launch", "gazebo.launch.py"
+            )
+        ]),
+        launch_arguments={"verbose": "true"}.items(),
+    )
+
+    # Spawn the robot in Gazebo Classic
+    spawn_robot = Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=[
+            "-entity", "ur5_robot",
+            "-topic", "robot_description"
+        ],
+        output="screen"
+    )
+
+    return LaunchDescription([
+        model_arg,
+        gazebo_resource_path,
+        robot_state_publisher_node,
+        gazebo_classic,
+        spawn_robot
+    ])
